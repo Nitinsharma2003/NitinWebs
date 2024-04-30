2f22b8d18ae206ac2a780f0971b95d0cb0a1934d
Nitinsharma2003 <131844328+Nitinsharma2003@users.noreply.github.com>
2024-04-30 11:52:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+document.getElementById('text1').addEventListener("click", () => {
+    window.open("Nitin Sharma (1).pdf", "_parent", "");
+})
+document.getElementById('text2').addEventListener("click", () => {
+    window.open("webdevelopmentquiz.pdf", "_parent", "");
+})
+document.getElementById('text4').addEventListener("click", () => {
+    window.open("Nitin Sharma python.pdf", "_parent", "");
+})
+document.getElementById('text3').addEventListener("click", () => {
+    window.open("flutter.pdf", "_parent", "");
+})
+document.getElementById('btn').addEventListener("click", () => {
+    window.open("RESUME (3).pdf", "_parent", "")
+})
+
+// let naam = document.getElementById('name1')
+// let form = document.getElementById('form1')
+// let text = document.getElementById('textarea')
+// form.addEventListener('click', (e) => {
+//     if (naam.value === "" || naam.value == null) {
+//         e.preventDefault();
+//         document.getElementById('res1').innerText = "Please fill this"
+//         document.getElementById('res1').style.color = 'green';
+//         document.getElementById('res1').style.fontSize = '20px';
+//     } else {
+//         document.getElementById('res1').innerText = " ";
+//     }
+
+//     if (text.value === "" || text.value == null) {
+//         e.preventDefault()
+//         document.getElementById('res2').innerText = "Fill this first";
+//     } else {
+//         document.getElementById('res2').innerText = " ";
+//     }
+// })
